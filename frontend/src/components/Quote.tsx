823f996f2a75d3e2e5aed88a74d5f11b95661eef
yash kelhe <yashkelhe30@gmail.com>
2024-07-27 17:03:29 +0530

    small changes

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,19 +1,16 @@
 const Quote = () => {
   return (
     <div className="bg-slate-200 h-screen flex justify-center flex-col">
-     <div className="flex justify-center">
-      <div className="max-w-md text-3xl font-bold text-center">
-         "The customer service I received was exceptional. The support team went above and beyond to address my concerns."
+      <div className="flex justify-center">
+        <div className="max-w-md text-3xl font-bold text-center">
+          "The customer service I received was exceptional. The support team
+          went above and beyond to address my concerns."
+        </div>
       </div>
-     </div>
-      <div className="max-w-md text-center text-lg font-bold font-mono mt-2">
-        Julies Williom
-      </div>
-      <div className="max-w-md text-center text-base font-bold font-mono mt-2 text-gray-600">
-        CEO || Acme corp
-      </div>
+      <div className="max-w-md text-center text-lg font-bold font-mono mt-2"></div>
+      <div className="max-w-md text-center text-base font-bold font-mono mt-2 text-gray-600"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Quote
+export default Quote;
